42a8a88eb62d07abbe68b39df76a94256eef1ee3
pedro-donoso <pedro.donoso.r@usach.cl>
2023-05-23 13:20:38 -0400

    agrego alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 
 })
+
+// enviar correo
+$(document).ready(function () {
+    $("#enviarCorreo").click(function () {
+        alert('El correo fue enviado correctamente...');
+    });
+});
